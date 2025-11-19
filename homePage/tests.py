48d5105f5db648e79a250a534235e61b86d58852
Kamal Yadav <133135982+offiicialkamal@users.noreply.github.com>
2025-11-19 13:08:56 +0000

    working

--- homePage/tests.py
+++ homePage/tests.py
@@ -1,3 +1,3 @@
-from django.test import TestCase
+from django.test import TestCase, testcases
 
 # Create your tests here.
--- homePage/views.py
+++ homePage/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.list import QuerySet
 
 # Create your views here.
 
+
 class homeView(TemplateView):
-    pass
+    template_name = 'homePage/home.html'
+    def get_context_data(self, **a):
+        context = super().get_context_data(**a)
+        context['school'] = "naresh it"
+        return context
